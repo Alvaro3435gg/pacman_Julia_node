1ceeb64acbee8f825aebab4cd0d9c0e76ac70284
Alvaro <alvaro.a.cruz.j@gmail.com>
2024-09-23 19:15:36 -0600

    initializated ghost with AStar

--- pacman.jl
+++ pacman.jl
@@ -1,4 +1,4 @@
-using Agents
+using Agents, Agents.Pathfinding
 using CairoMakie
 
 @agent struct Ghost(GridAgent{2})
@@ -33,13 +33,29 @@ function agent_step!(agent, model)
     end
 end
 
+
+function agent_step!(agent, model)
+    possibleMoves = nearby_positions(agent, model, 1)
+    
+    validMoves = [(x, y) for (x, y) in possibleMoves if matrix[x, y] == 1]
+    
+    if !isempty(validMoves)
+        new_position = rand(validMoves)  
+        move_agent!(agent, new_position, model) 
+    end
+end
+
+
 function initialize_model()
     dims = size(matrix)
+    target = (1, 1)
     space = GridSpace(dims; periodic = false, metric = :manhattan)
-
+    pathfinder = AStar(space, walkmap = matrix, diagonal_movement=false)
     model = StandardABM(Ghost, space; agent_step!)
-    return model
+    add_agent!(Ghost, pos=(8, 6), model)
+    plan_route!(model[1], target, pathfinder)
+    return model, pathfinder
 end
 
 model = initialize_model()
-a = add_agent!(Ghost, pos=(8, 6), model)
+add_agent!(Ghost, pos=(8, 6), model)
